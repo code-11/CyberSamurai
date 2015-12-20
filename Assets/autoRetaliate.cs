a1dc053842221ef20d6458d7bd50c23f3f48b6d7
code-11 <brendan.ritter@yahoo.com>
2015-12-19 23:03:51 -0500

    Added better enemy retaliation. Now enemies will slice at you if you bump into them.

--- Assets/autoRetaliate.cs
+++ Assets/autoRetaliate.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class autoRetaliate : MonoBehaviour {
 
 	public float m_retaliationDelay=1f;
+	public float m_strikeLength=1f;
 
 	// Use this for initialization
 	void Start () {
@@ -19,6 +20,11 @@ public class autoRetaliate : MonoBehaviour {
 	{
 		if(col.gameObject.tag == "Player")
 		{
+			Vector2 hitLoc = col.contacts [0].point;
+			Vector2 hitDir = hitLoc - (Vector2) transform.position;
+			Debug.Log (hitDir);
+			StartCoroutine (sliceDelay (hitDir));
+			/*
 			LifeCtrl playerLife = col.gameObject.GetComponent<LifeCtrl> ();
 			if (playerLife != null) {
 				playerLife.dcrHealth ();
@@ -26,22 +32,22 @@ public class autoRetaliate : MonoBehaviour {
 			} else {
 				Debug.Log ("Player has no life module!");
 			}
+			*/
 		}
 	}
-	IEnumerator sliceDelay() {
+	IEnumerator sliceDelay(Vector2 hitLoc) {
 		yield return new WaitForSeconds(m_retaliationDelay);
-
+		enemySlice (hitLoc);
 	}
-	/*
-	private void enemySlice(){
-		Vector2 forward = moveRef.getLastHeading ();
-		Debug.Log (forward);
-		RaycastHit2D [] hits = Physics2D.RaycastAll (transform.position, forward, m_strikeLength);
-		Vector2 there = (((Vector2) transform.position) + (forward*m_strikeLength));
-		Debug.DrawLine (transform.position, there , Color.red,.5f);
+
+	private void enemySlice(Vector2 hitLoc){
+		hitLoc.Normalize ();
+		RaycastHit2D [] hits = Physics2D.RaycastAll (transform.position, hitLoc, m_strikeLength);
+		Vector2 there = (((Vector2) transform.position) + (hitLoc*m_strikeLength));
+		Debug.DrawLine (transform.position, there , Color.red,m_strikeLength);
 
 		foreach (var hit in hits) {
-			if (hit.collider.gameObject.tag != "Player") {
+			if (hit.collider.gameObject.tag == "Player") {
 				LifeCtrl hitLife = hit.collider.gameObject.GetComponent<LifeCtrl> ();
 				if (hitLife != null) {
 					hitLife.dcrHealth ();
@@ -49,5 +55,5 @@ public class autoRetaliate : MonoBehaviour {
 			}
 		}
 	}
-	*/
+
 }
